35907a445ead6a884e89954d8e37eb3759dd4a71
gabustoledo <gabriel.bustamante@usach.cl>
2021-05-03 20:29:57 -0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Operaciones from './Operaciones.js'
+
+
+function App() {
+    return (
+
+        <div className="App d-flex justify-content-center align-items-center">
+
+            <Operaciones/>
+
+        </div>
+    );
+}
+
+export default App;
